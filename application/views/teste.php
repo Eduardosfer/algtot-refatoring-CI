50cbcd5b41a733372849fa541a750afcc3ef2c76
Eduardosfer <eduardosfer@outlook.com>
2018-12-22 20:50:27 -0300

    Adição do htaccess para URLs amigaveis

---
+++ application/views/teste.php
@@ -0,0 +1,15 @@
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>teste</title>
+</head>
+
+<body>
+    <h1>Teste de separação: <?= site_url('novo'); ?></h1>
+    <a href="<?= site_url('novo'); ?>">Clica</a>
+</body>
+
+</html>
